38466d46a2e4c35f059a049b85ecbd0bb55eda07
logan <logan.ragland25@gmail.com>
2024-03-03 15:44:04 -0500

    Created random password generator

--- First Project/main.cpp
+++ First Project/main.cpp
@@ -1,9 +1,17 @@
 //Import iostream for input/output
 #include <iostream>
+#include <string>
 
 int main() {
-    for (int x = 0; x < 22; x++) {
-        std::cout << "Kayley says hi" << std::endl;
-    }
+    std::string name;
+    int age;
+
+    std::cout << "Enter your name" << std::endl;
+    std::getline(std::cin, name);
+
+    std::cout << "Enter your age" << std::endl;
+    std::cin >> age;
+
+    std::cout << name << ", you are " << age << " years old" << std::endl;
     return 0;
-}
+} 
---
+++ Password Creator/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <fstream>
+
+int main() {
+    //Declare Variables
+    ofstream PasswordsFile("Password Creator\\passwords.txt", ios::app);
+    string characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*_-+=<,>.?/";
+
+    string sitename;
+    string username;
+    string pLengthInput;
+    int pLength;
+    string password = "";
+
+    //Get Name of Site
+    cout << "Please enter the name of the website or software:\n\t-";
+    getline(cin, sitename);
+
+    //Get Username
+    cout << "\nPlease enter your username:\n\t-";
+    getline(cin, username);
+
+    //Get Password Length
+    cout << "\nPlease enter the length of your password (Must be between 8 and 32 characters):\n\t";
+    getline(cin, pLengthInput);
+    try { pLength = stoi(pLengthInput); }
+    catch (exception e) { }
+    
+    //Make sure password meets requested length and is not a string
+    if (pLength < 8 || pLength > 32) {
+        while (true) {
+            cout << "\nPlease match the requested format of an integer between 8 and 32 characters):\n\t-";
+            getline(cin, pLengthInput);
+            try { 
+                pLength = stoi(pLengthInput);
+                if (pLength >= 8 && pLength <= 32) {
+                    break;
+                }
+            }
+            catch (exception e) {
+                cout << "Please enter a number!" << endl;
+            }
+        }
+    }
+
+    //Seed random value
+    srand((unsigned) time(NULL));
+    
+    //Create password from characters acoording to inputed length
+    for (int pos = 0; pos < pLength; pos++) { password += characterList[rand() % characterList.length()]; }
+
+    //Print data into file then close it
+    PasswordsFile << "Site: " << sitename << "\nUsername: " << username << "\nPassword: " << password << "\n" << endl;
+    PasswordsFile.close();
+
+    //Notify user password successfully generated
+    cout << "\nSuccess! Password is " << password << endl;
+}
